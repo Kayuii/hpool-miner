973ab466c515818e48bfe86e8afbbe531870649f
Kayuii <577738@qq.com>
2021-06-22 13:54:18 +0800

    add config.yaml for aarch64 and arm
    Signed-off-by: Kayuii <577738@qq.com>

--- hpool/miner-v1.4.1-1/docker-entrypoint.sh
+++ hpool/miner-v1.4.1-1/docker-entrypoint.sh
@@ -6,21 +6,40 @@ if [ $(echo "$1" | cut -c1) = "-" ]; then
     set -- hpool-chia-miner "$@"
 fi
 
+if [ ! -f config.yaml ]; then
+cat <<-EOF > "/opt/config.yaml"
+path:
+minerName:
+apiKey:
+cachePath: ""
+deviceId: ""
+extraParams: {}
+log:
+  lv: info
+  path: ./log/
+  name: miner.log
+url:
+  proxy: ""
+scanPath: false
+scanMinute: 60
+EOF
+fi
+
 if [ "$1" = "hpool-chia-miner" ] ; then
     if [ -n "$DIR" ]; then
-        echo "$(sed "/path:$/c path: $DIR" config.yaml)" > config.yaml 
+        echo "$(sed "/path:$/c path: $DIR" config.yaml)" > config.yaml
     fi
     if [ -n "$APIKEY" ]; then
-        echo "$(sed "/apiKey:$/c apiKey: $APIKEY" config.yaml)" > config.yaml 
+        echo "$(sed "/apiKey:$/c apiKey: $APIKEY" config.yaml)" > config.yaml
     fi
     if [ -n "$HOSTNAME" ]; then
-        echo "$(sed "/minerName:$/c minerName: $HOSTNAME" config.yaml)" > config.yaml 
+        echo "$(sed "/minerName:$/c minerName: $HOSTNAME" config.yaml)" > config.yaml
     fi
     if [ -n "$PROXY" ]; then
-        echo "$(sed "s!proxy: \"\"!proxy: \"$PROXY\"!g" config.yaml)" > config.yaml 
+        echo "$(sed "s!proxy: \"\"!proxy: \"$PROXY\"!g" config.yaml)" > config.yaml
     fi
     if [ -n "$LOGPATH" ]; then
-        echo "$(sed "s:./log/:$LOGPATH:g" config.yaml)" > config.yaml 
+        echo "$(sed "s:./log/:$LOGPATH:g" config.yaml)" > config.yaml
         mkdir -p "$LOGPATH"
         chown -R chia "$LOGPATH"
         chown -h chia:chia "$LOGPATH"
@@ -30,8 +49,8 @@ if [ "$1" = "hpool-chia-miner" ] ; then
         chown -h chia:chia ./log/
     fi
     if [ -n "$SCAN" ]; then
-        echo "$(sed "/scanPath/c scanPath: true" config.yaml)" > config.yaml 
-        echo "$(sed "/scanMinute/c scanMinute: $SCAN" config.yaml)" > config.yaml 
+        echo "$(sed "/scanPath/c scanPath: true" config.yaml)" > config.yaml
+        echo "$(sed "/scanMinute/c scanMinute: $SCAN" config.yaml)" > config.yaml
     fi
     chown -R chia .
     chown -h chia:chia .
