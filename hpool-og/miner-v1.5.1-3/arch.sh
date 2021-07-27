bcb165cd278f98ce4de74391252ed0deb3294631
Kayuii <577738@qq.com>
2021-07-27 16:38:58 +0800

    update hpool og v1.5.1-3
    Signed-off-by: Kayuii <577738@qq.com>

--- hpool-og/miner-v1.5.1-3/arch.sh
+++ hpool-og/miner-v1.5.1-3/arch.sh
@@ -3,5 +3,5 @@ if [ "$TARGETARCH" = "amd64" ] ; then ARCH=linux;fi
 if [ "$TARGETARCH" = "arm64" ] ; then ARCH=aarch64;fi
 if [ "$TARGETARCH" = "arm" ] ; then ARCH=arm;fi
 VER=$1
-echo "https://github.com/hpool-dev/chia-miner/releases/download/1.5.1/HPool-Miner-chia-v1.5.1-2-${ARCH}.zip "
-wget -q --no-check-certificate https://github.com/hpool-dev/chia-miner/releases/download/1.5.1/HPool-Miner-chia-v1.5.1-2-${ARCH}.zip -O /tmp/chia-miner.zip && unzip -j /tmp/chia-miner.zip -d /tmp/linux
+echo "https://github.com/hpool-dev/chia-miner/releases/download/1.5.1/HPool-Miner-chia-v1.5.1-3-${ARCH}.zip "
+wget -q --no-check-certificate https://github.com/hpool-dev/chia-miner/releases/download/1.5.1/HPool-Miner-chia-v1.5.1-3-${ARCH}.zip -O /tmp/chia-miner.zip && unzip -j /tmp/chia-miner.zip -d /tmp/linux
