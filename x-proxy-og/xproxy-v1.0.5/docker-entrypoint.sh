0fcfbf424db0926a01333bdcb3c4aef6078afbd9
Kayuii <577738@qq.com>
2021-07-27 18:29:17 +0800

    fix xproxy og
    Signed-off-by: Kayuii <577738@qq.com>

--- x-proxy-og/xproxy-v1.0.5/docker-entrypoint.sh
+++ x-proxy-og/xproxy-v1.0.5/docker-entrypoint.sh
@@ -5,15 +5,45 @@ if [ $(echo "$1" | cut -c1) = "-" ]; then
     echo "$0: assuming arguments for x-proxy"
     set -- x-proxy "$@"
 fi
+if [ ! -f config.yaml ]; then
+cat <<-EOF > "/opt/config.yaml"
+server:
+  host: 0.0.0.0
+  port: 9190
+  url: ""
+log:
+  level: "info"
+  out: "./log/proxy.log"
+dbFile: "proxy.db"
+dbFile: "/opt/db/proxy.db"
+chains:
+  -
+    chain: mass
+    apiKey: "mass"
+  -
+    chain: chia
+    apiKey: "chia"
+EOF
+fi
 
 if [ "$1" = "x-proxy" ] ; then
+    mkdir -p /opt/log/ /opt/db/
 
     if [ -n "$LOGPATH" ]; then
         sed -i "s:./log/:$LOGPATH:g" config.yaml
     else
-        LOGPATH=./log/
+        LOGPATH=/opt/log/
         sed -i "s:./log/:$LOGPATH:g" config.yaml
     fi
+    if [ -n "$DBDIR" ]; then
+        sed -i "s:./log/:$DBDIR:g" config.yaml
+    else
+        DBDIR=/opt/db/proxy.db
+        sed -i "s:proxy.db:$DBDIR:g" config.yaml
+    fi
+
+    sed -i "s:apiKey: \"mass\":apiKey: \"$MASSAPI\":g" config.yaml
+    sed -i "s:apiKey: \"chia\":apiKey: \"$CHIAAPI\":g" config.yaml
 
     cat config.yaml
     chown -R chia ./
