da9ac6593ff423be1268341134c8a6b4f187f0ff
Kayuii <577738@qq.com>
2021-10-18 14:49:10 +0800

    update hpool og v1.5.5-1
    Signed-off-by: Kayuii <577738@qq.com>

--- hpool-og/miner-v1.5.5-1/arch.sh
+++ hpool-og/miner-v1.5.5-1/arch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# if [ "$TARGETARCH" = "amd64" ] ; then ARCH=linux;fi
-# if [ "$TARGETARCH" = "arm64" ] ; then ARCH=aarch64;fi
-# if [ "$TARGETARCH" = "arm" ] ; then ARCH=arm;fi
+if [ "$TARGETARCH" = "amd64" ] ; then ARCH=linux;fi
+if [ "$TARGETARCH" = "arm64" ] ; then ARCH=aarch64;fi
+if [ "$TARGETARCH" = "arm" ] ; then ARCH=arm;fi
 VER=$1
-ARCH=linux
-echo "https://github.com/hpool-dev/chia-miner/releases/download/1.5.5/HPool-Miner-chia-og-v1.5.5-1-${ARCH}.zip "
-wget -q --no-check-certificate https://github.com/hpool-dev/chia-miner/releases/download/1.5.5/HPool-Miner-chia-og-v1.5.5-1-${ARCH}.zip -O /tmp/chia-miner.zip && unzip -j /tmp/chia-miner.zip -d /tmp/linux
+# ARCH=linux
+echo "https://github.com/hpool-dev/chia-miner/releases/download/1.5.5/HPool-Miner-chia-og-v1.5.5-1-linux.zip "
+wget -q --no-check-certificate https://github.com/hpool-dev/chia-miner/releases/download/1.5.5/HPool-Miner-chia-og-v1.5.5-1-linux.zip -O /tmp/chia-miner.zip && unzip -j /tmp/chia-miner.zip "linux/hpool-miner-chia-linux-${ARCH}" -d /tmp/linux
