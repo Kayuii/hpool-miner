6f09eccd0b2a748d59f6203c48b4e172db7c0d95
kayuii <577738@qq.com>
2022-01-05 11:41:16 +0800

    update hpool pp xproxy v1.0.7

--- x-proxy-pp/xproxy-v1.0.7/arch.sh
+++ x-proxy-pp/xproxy-v1.0.7/arch.sh
@@ -3,5 +3,5 @@ if [ "$TARGETARCH" = "amd64" ] ; then ARCH=linux;fi
 if [ "$TARGETARCH" = "arm64" ] ; then ARCH=aarch64;fi
 if [ "$TARGETARCH" = "arm" ] ; then ARCH=arm;fi
 VER=$1
-echo "https://github.com/hpool-dev/chiapp-miner/releases/download/${VER}/chiapp-xproxy-pp-v1.0.7-3.zip"
-wget -q --no-check-certificate https://github.com/hpool-dev/chiapp-miner/releases/download/${VER}/chiapp-xproxy-pp-v1.0.7-3.zip -O /tmp/chia-miner.zip && unzip -j /tmp/chia-miner.zip -d /tmp/linux
+echo "https://github.com/hpool-dev/chiapp-miner/releases/download/1.5.3/chiapp-xproxy-pp-v1.0.7-3.zip"
+wget -q --no-check-certificate https://github.com/hpool-dev/chiapp-miner/releases/download/1.5.3/chiapp-xproxy-pp-v1.0.7-3.zip -O /tmp/chia-miner.zip && unzip -j /tmp/chia-miner.zip -d /tmp/linux
